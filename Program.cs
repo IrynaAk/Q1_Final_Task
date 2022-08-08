e550f15e1a2791a1e8823c4a94ff4e8723c108fe
Iryna_Akunevich <akunevichirina@mail.ru>
2022-08-08 14:02:01 +0300

    Code with solution added

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// написать программу, которая из имеющегося массива строк формирует массив строк длина которых меньше
+// либо равна 3 символам. Первоначальный массив нужно ввести с клавиатуры, либо задать на старте выполнения 
+// алгоритма
+
+using System;
+using static System.Console;
+Clear();
+
+string[] M; 
+WriteLine("Please type strings separated by spaces:");
+string initialString = ReadLine();
+M = initialString.Split(' ');
+string[] result = new string[M.Length];
+int size = 0;
+foreach (var value in M)
+{
+    if(value.Length <=3)
+    {
+        result[size] = value;
+        size++;
+    }
+}
+WriteLine(string.Join(" ", result, 0, size));
+
+
+
+
+
+
+
+
+
